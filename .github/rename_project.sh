c3ce721b806aeadb374d40f1e99dcbe5e2e54929
elliez <elliez@users.noreply.github.com>
2021-11-22 22:15:39 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="elliez"
+original_name="simple_package"
+original_urlname="simple_package"
+original_description="Awesome simple_package created by elliez"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv simple_package $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,6 @@
 import pytest
 
-from project_name import BaseClass, base_function
+from simple_package import BaseClass, base_function
 
 given = pytest.mark.parametrize
 
